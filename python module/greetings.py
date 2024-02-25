351bc82654ea0493c82499fa8dfe9159a8f47764
frenki4141 <frankbaraka589@gmail.com>
2024-02-25 21:44:45 +0300

    first commit

---
+++ python module/greetings.py
@@ -0,0 +1,14 @@
+# user_input.py
+
+# Ask for user's name
+name = input("frank baraka: ")
+
+# Ask for user's age
+age = input("23: ")
+
+# Ask for user's location
+location = input("nairobi: ")
+
+# Print personalized message
+print(f"Hello {"frank baraka:"}, i am {23} years old and live in {"nairobi:"}.")
+
